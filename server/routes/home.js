9f09b2744624bcf18a434184f988d34bc6ee5d98
addupe <autumndupin@gmail.com>
2020-01-23 19:42:31 -0600

    working home recording route

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,9 +1,18 @@
 // home (explore) routes
 const express = require('express');
+const { getHomeRecordings } = require('../db/helpers/home');
 const homeRouter = express.Router();
 
-homeRouter.get('/', (req, res) => {
-  res.send('lets explore!');
+homeRouter.get('', (req, res) => {
+  getHomeRecordings()
+  .then(rows => {
+    res.send(rows);
+  })
+  .catch(err => {
+    debugger;
+    res.sendStatus(404);
+  });
+
 })
 
 module.exports = homeRouter;
