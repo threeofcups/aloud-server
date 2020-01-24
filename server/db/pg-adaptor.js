b70d31155265680971e41ecdafca3b1cae9f528b
addupe <autumndupin@gmail.com>
2020-01-24 14:38:41 -0600

    modify pg-adaptor for local db connection

--- server/db/pg-adaptor.js
+++ server/db/pg-adaptor.js
@@ -3,17 +3,17 @@ const pgPromise = require('pg-promise');
 const promisify = require('util.promisify');
 
 const pgp = pgPromise({}); // Empty object means no additional config required
-const db_user = process.env.POSTGRES_USER || 'postgres';
-const db_password = process.env.POSTGRES_PASSWORD || 'postgres';
-const db_name = process.env.POSTGRES_DB || 'aloud_local';
-const db_host = process.env.POSTGRES_HOST || 'localhost';
+const db_user = process.env.POSTGRES_USERx || 'postgres';
+const db_password = process.env.POSTGRES_PASSWORDx || 'postgres';
+const db_name = process.env.POSTGRES_DBx || 'aloud_local';
+const db_host =  'localhost';
+// const db_host = `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}` || 'localhost';
 
 const config = {
   user: db_user,
   password: db_password,
   database: db_name,
   host: db_host,
-  socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`
 };
 
 const db = pgp(config);
