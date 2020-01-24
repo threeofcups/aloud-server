b3baf74491210f2bb96d8e2ccd98dfe176b23edf
addupe <autumndupin@gmail.com>
2020-01-23 19:42:43 -0600

    working inner join recordings, users query

--- server/db/helpers/home.js
+++ server/db/helpers/home.js
@@ -1,13 +1,13 @@
-const { query } = require('../../db/pg-adaptor');
+const { db } = require('../../db/pg-adaptor');
+
 
 const getHomeCollections = () => {
-  return query();
+  // return query();
 };
 
 const getHomeRecordings = () => {
-  const recordingSQL = 'select * from recordings'
-  return query(recordingSQL);
-
+  const recordingSQL = `SELECT recordings.id, username, name_display, title, description, url_recording, speech_to_text, created_at FROM recordings INNER JOIN users ON users.id = recordings.id_user WHERE published = 'public'`;
+  return db.query(recordingSQL);
 };
 
 module.exports.getHomeCollections = getHomeCollections;
