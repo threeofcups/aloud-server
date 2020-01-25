2a10f8a5b9f33663a98b3b8db042829fc85da1b5
Jill Poole <jillianntish@gmail.com>
2020-01-25 11:01:42 -0600

    setup cloudinary connection and structure

--- server/api/cloudinary.js
+++ server/api/cloudinary.js
@@ -1,2 +1,13 @@
 const cloudinary = require('cloudinary').v2;
 
+cloudinary.config({ 
+  cloud_name: 'dahfjsacf', 
+  api_key: '235725224111251', 
+  api_secret: 'uRQkBgBWl5qSLzUIQHfOqzeZzak' 
+});
+
+/*cloudinary
+.uploader.upload("my_image.jpg", function(error, result) {
+  console.log(result, error)
+});
+*/
