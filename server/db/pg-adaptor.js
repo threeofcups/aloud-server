1c16940c059e65b5f625dd0d8f4b1e0a7bcc9201
Jill Poole <jillianntish@gmail.com>
2020-01-25 10:54:40 -0600

    reset db config obj

--- server/db/pg-adaptor.js
+++ server/db/pg-adaptor.js
@@ -3,22 +3,18 @@ const pgPromise = require('pg-promise');
 const promisify = require('util.promisify');
 
 const pgp = pgPromise({}); // Empty object means no additional config required
-const db_user = process.env.POSTGRES_USER || 'postgres';
-const db_password = process.env.POSTGRES_PASSWORD || 'postgres';
-const db_name = process.env.POSTGRES_DB || 'aloud_local';
-const db_host = process.env.POSTGRES_HOST || 'localhost';
 
 const config = {
-  user: db_user,
-  password: db_password,
-  database: db_name,
-  host: db_host,
+  user: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
+  database: process.env.POSTGRES_DB || 'aloud',
+  host: process.env.POSTGRES_HOST || 'localhost',
   socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`
 };
 
 const db = pgp(config);
 const query = promisify(db.query).bind(db);
 
-db.connect().then(console.log(`connected to ${db_name}`));
+query('select * from users;').then(console.log(`Connected to aloud db`));
 
-module.exports.db = db;
+exports.db = db;
