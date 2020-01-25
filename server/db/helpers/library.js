2d1e0a072b633da1c4b3b84c2c9c48e806cdb6c4
addupe <autumndupin@gmail.com>
2020-01-25 14:16:38 -0600

    cleanup helper code

--- server/db/helpers/library.js
+++ server/db/helpers/library.js
@@ -1,16 +1,6 @@
 const { db } = require('../../db/pg-adaptor');
 
 const stageLibraryContent = async (userId) => {
-
-  // const creatorCollectionUsername = `SELECT u.username FROM users WHERE u.id IN ($ids)`
-  // const collectionSQL = `SELECT usc.id_user, usc.id_collection, usc.created_at, c.id_user_creator, c.title, c.description, c.url_image, c.count_recordings, u.username FROM users_saved_collections as usc JOIN users as u ON u.id = usc.id_user JOIN collections as c ON c.id = usc.id_collection WHERE usc.id_user = ${userId}`;
-  // const recordingSQL = `SELECT usr.id_user, usr.id_recording, usr.created_at, r.title, r.description, r.url_recording, r.speech_to_text, r.created_at, r.id_user, u.username FROM users_saved_recordings as usr JOIN users as u ON u.id = usr.id_user JOIN recordings as r ON r.id = usr.id_recording WHERE usr.id_user = ${userId} AND r.published = 'public'`;
-
-  // get all user’s saved collection ids first
-  // save as variable
-  // then get all the info from those collections
-  // same w recordings
-  
   
   const assignContentIds = async(userId) => {
     const collectionSQL = `SELECT id_collection FROM users_saved_collections WHERE id_user = ${userId}`;
