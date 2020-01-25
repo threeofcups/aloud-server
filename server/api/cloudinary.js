5cc7551e7069a077aa4b5d4163f0f071a81f60ba
Jill Poole <jillianntish@gmail.com>
2020-01-25 11:05:44 -0600

    fix cloudinary request structure

--- server/api/cloudinary.js
+++ server/api/cloudinary.js
@@ -6,8 +6,8 @@ cloudinary.config({
   api_secret: 'uRQkBgBWl5qSLzUIQHfOqzeZzak' 
 });
 
-/*cloudinary
-.uploader.upload("my_image.jpg", function(error, result) {
-  console.log(result, error)
-});
-*/
+// cloudinary.uploader.upload("dog.mp4", 
+//   { resource_type: "video", 
+//     public_id: "my_folder/my_sub_folder/dog_closeup",
+//     chunk_size: 6000000,
+//   function(error, result) {console.log(result, error)});
