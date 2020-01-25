7451c478ad6f101178d9c264f323c1996e20c041
addupe <autumndupin@gmail.com>
2020-01-25 14:14:43 -0600

    fix bug with collection images

--- server/db/helpers/library.js
+++ server/db/helpers/library.js
@@ -24,8 +24,8 @@ const stageLibraryContent = async (userId) => {
   };
 
   const stageContent = async(ids, userId) => {
-    const collectionSQL = `SELECT *, u.username FROM collections JOIN users AS u ON u.id = id_user_creator WHERE id_user_creator != ${userId}`;
-    const recordingSQL = `SELECT *, u.username FROM recordings JOIN users AS u ON u.id = id_user WHERE id_user != ${userId}`;
+    const collectionSQL = `SELECT c.id, title, description, count_recordings, c.url_image, c.created_at, u.username FROM collections as c JOIN users AS u ON u.id = id_user_creator WHERE id_user_creator != ${userId}`;
+    const recordingSQL = `SELECT r.id, title, description, url_recording, speech_to_text, r.created_at, u.username FROM recordings as r JOIN users AS u ON u.id = id_user WHERE id_user != ${userId}`;
 
     const content = [{
       collections: await db.query(collectionSQL),
