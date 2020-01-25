933c31b188031735a9a2556d3aeda3f7e8b56df7
addupe <autumndupin@gmail.com>
2020-01-25 09:49:43 -0600

    write routes for profile content grab

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,9 +1,18 @@
 // profile routes
 const express = require('express');
 const profileRouter = express.Router();
+const { getAllProfileContent } = require('../db/helpers/profile');
 
-profileRouter.get('/', (req, res) => {
-  res.send('nice profile you got');
+profileRouter.get('/:username/:userId', (req, res) => {
+  const { userId } = req.params;
+  
+  getAllProfileContent(userId)
+  .then(profileContent => {
+    res.send(profileContent);
+  })
+  .catch(err => {
+    res.sendStatus(404);
+  });
 });
 
 module.exports = profileRouter;
