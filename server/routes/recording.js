0392c83062982dbeb8ee65182ca7c4a26f1d9b55
addupe <autumndupin@gmail.com>
2020-01-27 19:35:38 -0600

    add route for saving new recoridng to db

--- server/routes/recording.js
+++ server/routes/recording.js
@@ -1,9 +1,37 @@
 //recording routes
 const express = require('express');
+const { saveRecording } = require('../db/helpers/recording');
 const recordingRouter = express.Router();
 
 recordingRouter.get('/', (req, res) => {
   res.send('reccy reccy reccy');
+});
+
+recordingRouter.post('/save', (req, res) => {
+  const { body } = req;
+
+  // {
+  //   "id_user": "1",
+  //     "title": "sample_title",
+  //       "description": "sample_description",
+  //         "url_recording": "cloudinary.mp3",
+  //           "published": "public",
+  //             "speech_to_text": "sample sample sample",
+  // }
+
+  // body;
+  // debugger;
+
+  saveRecording(body)
+  .then(success => {
+    res.sendStatus(201);
+  })
+  .catch(err => {
+    console.error(err);
+    debugger;
+    res.sendStatus(400);
+  })
+
 });
 
 module.exports = recordingRouter;
