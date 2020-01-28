8078bc106ecd151348d4b240836135ae20fb492a
addupe <autumndupin@gmail.com>
2020-01-28 12:29:39 -0600

    add helper to retrieve recording info belonging to collection

--- server/db/helpers/collection.js
+++ server/db/helpers/collection.js
@@ -13,5 +13,12 @@ const saveCollection = (collectionBody) => {
   const insertCollection = `INSERT INTO collections(id_user_creator, title, description, count_recordings, url_image, created_at) VALUES('${id_user_creator}', '${title}', '${description}', '0', '${url_image}', now())`;
   return db.query(insertCollection);
 };
+ 
+//get all recordings info that belong to a collection
+const getCollectionRecordings = (collectionId) => {
+  const joinSQL = `SELECT * FROM collections_recordings JOIN recordings as r ON id_recording = r.id WHERE id_collection = ${collectionId}`;
+  return db.query(joinSQL);
+}
 
 module.exports.saveCollection = saveCollection;
+module.exports.getCollectionRecordings = getCollectionRecordings;
