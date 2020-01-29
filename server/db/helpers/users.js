4994c36607565a1a6cacee9e8b9bc3c24d6602ac
addupe <autumndupin@gmail.com>
2020-01-29 14:40:01 -0600

    add helper to query db for userId from google id

--- server/db/helpers/users.js
+++ server/db/helpers/users.js
@@ -1,9 +1,17 @@
 const {db} = require('../../db/pg-adaptor');
 
+
+  //query to get userId from googleId for queries throughout the app
+  //userId should be added to global context
+  //requests should be made with this, not with the googleId
+const getUserId = (googleId) => {
+  const googSQL = `SELECT id FROM users WHERE id_google = '${googleId}'`;
+  return db.query(googSQL);
+};
+
 const saveNewUser = (userObject) => {
   const { id, name, email, familyName, givenName, photoUrl } = userObject;
   const username = email.replace('@gmail.com', '');
-  debugger;
   //default bio
   const bio = ' ';
 
@@ -15,3 +23,4 @@ const saveNewUser = (userObject) => {
 //image, bio, name, username
 
 module.exports.saveNewUser = saveNewUser;
+module.exports.getUserId = getUserId;
