5a78d918b953e4af537637b2efcb1617a7f22655
addupe <autumndupin@gmail.com>
2020-01-31 15:16:06 -0600

    add recently saved to home route

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,12 +1,13 @@
 // home (explore) routes
 const express = require('express');
-const { getHomeRecordings, getHomeCollections } = require('../db/helpers/home');
+const { getHomeRecordings, getHomeCollections, getRecentlySaved } = require('../db/helpers/home');
 const homeRouter = express.Router();
 
 homeRouter.get('/:userId', (req, res) => {
   const { userId } = req.params;
 
   const homeContent = [{
+    recent: [],
     collections: [],
     recordings: [],
   }];
@@ -17,7 +18,11 @@ homeRouter.get('/:userId', (req, res) => {
     getHomeCollections(userId)
     .then(rows => {
       homeContent[0].collections = rows;
-      res.send(homeContent);
+      getRecentlySaved(userId)
+      .then(rows => {
+        homeContent[0].recent = rows;
+        res.send(homeContent);
+      })
     });
   })
   .catch(err => {
