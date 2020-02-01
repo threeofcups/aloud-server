81f6198241b101103b0b3c55a198ba39ac097884
addupe <autumndupin@gmail.com>
2020-01-31 18:26:19 -0600

    fix order by created at bug

--- server/db/helpers/home.js
+++ server/db/helpers/home.js
@@ -15,7 +15,7 @@ const getHomeRecordings = (userId) => {
 const stageRecentlySaved = async (userId) => {
 
   const assignContentIds = async (userId) => {
-    const collectionSQL = `SELECT id_collection FROM users_saved_collections WHERE id_user = ${userId} ORDER BY created_at`;
+    const collectionSQL = `SELECT id_collection FROM users_saved_collections WHERE id_user = ${userId}`;
     const ids = {
       collectionIds: await db.query(collectionSQL),
     };
@@ -27,7 +27,7 @@ const stageRecentlySaved = async (userId) => {
 
 
   const stageContent = async (ids, userId) => {
-    const collectionSQL = `SELECT c.id, title, description, count_recordings, c.url_image, c.created_at, u.username FROM collections as c JOIN users AS u ON u.id = id_user_creator WHERE id_user_creator != ${userId} AND c.id = ANY($1::int[])`;
+    const collectionSQL = `SELECT c.id, title, description, count_recordings, c.url_image, c.created_at, u.username FROM collections as c JOIN users AS u ON u.id = id_user_creator WHERE id_user_creator != ${userId} AND c.id = ANY($1::int[]) ORDER BY c.created_at DESC`;
 
     const content = [{
       collections: await db.query(collectionSQL, [ids.collectionIds]),
