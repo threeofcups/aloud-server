a51044c19bc4336d4b0eeaa911f3b1a597605fbf
addupe <autumndupin@gmail.com>
2020-02-03 19:03:03 -0600

    retrieve user ids from library queries

--- server/db/helpers/library.js
+++ server/db/helpers/library.js
@@ -18,8 +18,8 @@ const stageLibraryContent = async (userId) => {
 
 
   const stageContent = async(ids, userId) => {
-    const collectionSQL = `SELECT c.id, title, description, count_recordings, c.url_image, c.created_at, u.username FROM collections as c JOIN users AS u ON u.id = id_user_creator WHERE id_user_creator != ${userId} AND c.id = ANY($1::int[])`;
-    const recordingSQL = `SELECT r.id, title, description, url_recording, speech_to_text, r.created_at, u.username FROM recordings as r JOIN users AS u ON u.id = id_user WHERE id_user != ${userId} AND r.id = ANY($1::int[])`;
+    const collectionSQL = `SELECT c.id, id_user_creator, title, description, count_recordings, c.url_image, c.created_at, u.username FROM collections as c JOIN users AS u ON u.id = id_user_creator WHERE id_user_creator != ${userId} AND c.id = ANY($1::int[])`;
+    const recordingSQL = `SELECT r.id, r.id_user, title, description, url_recording, speech_to_text, r.created_at, u.username FROM recordings as r JOIN users AS u ON u.id = id_user WHERE id_user != ${userId} AND r.id = ANY($1::int[])`;
 
     const content = [{
       collections: await db.query(collectionSQL, [ids.collectionIds]),
