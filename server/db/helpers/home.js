14b3fb925504f8f61c12e8c9f9b9a534f90580be
addupe <autumndupin@gmail.com>
2020-02-03 19:01:27 -0600

    retrieve user ids for recently saved

--- server/db/helpers/home.js
+++ server/db/helpers/home.js
@@ -27,7 +27,7 @@ const stageRecentlySaved = async (userId) => {
 
 
   const stageContent = async (ids, userId) => {
-    const collectionSQL = `SELECT c.id, title, description, count_recordings, c.url_image, c.created_at, u.username FROM collections as c JOIN users AS u ON u.id = id_user_creator WHERE id_user_creator != ${userId} AND c.id = ANY($1::int[]) ORDER BY c.created_at DESC`;
+    const collectionSQL = `SELECT c.id, id_user_creator, title, description, count_recordings, c.url_image, c.created_at, u.username FROM collections as c JOIN users AS u ON u.id = id_user_creator WHERE id_user_creator != ${userId} AND c.id = ANY($1::int[]) ORDER BY c.created_at DESC`;
 
     const content = [{
       collections: await db.query(collectionSQL, [ids.collectionIds]),
