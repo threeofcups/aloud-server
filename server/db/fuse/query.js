24f03afae3305e596a62bedb3a0814a57676a6b1
addupe <autumndupin@gmail.com>
2020-02-06 11:56:57 -0600

    add collection stage and fuse to fuse-helpers

--- server/db/fuse/query.js
+++ server/db/fuse/query.js
@@ -4,12 +4,16 @@ const { getAllUsers } = require('../helpers/users');
 const { retrieveRecordingObjects } = require('../helpers/recording');
 const { retrieveCollectionObjects } = require('../helpers/collection');
 
-const stageQueryData = async() => {
+const stageRecordingData = async() => {
   return await retrieveRecordingObjects()
 };
 
-const fuser = async() => {
-  const recordings = await stageQueryData();
+const stageCollectionData = async () => {
+  return await retrieveCollectionObjects()
+};
+
+const recordingFuser = async() => {
+  const recordings = await stageRecordingData();
   const options = {
     shouldSort: true,
     includeScore: true,
@@ -29,5 +33,28 @@ const fuser = async() => {
   return fuse;
 };
 
-module.exports.stageQueryData = stageQueryData;
-module.exports.fuser = fuser;
+const collectionFuser = async () => {
+  const collections = await stageCollectionData();
+  const options = {
+    shouldSort: true,
+    includeScore: true,
+    includeMatches: true,
+    threshold: 0.4,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 3,
+    keys: [
+      "title",
+      "description",
+      "name",
+      "username"
+    ]
+  }
+
+  const fuse = new Fuse(collections, options);
+  return fuse;
+};
+
+module.exports.recordingFuser = recordingFuser;
+module.exports.collectionFuser = collectionFuser;
